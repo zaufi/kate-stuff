b5c4952b45c28e5933d4c8adb580c07ad53a7b90
Alex Turbov <i.zaufi@gmail.com>
2011-07-27 11:52:17 +0400

    Add gcc predefined macros snippets file (and script I used to gather all that predefines)

---
+++ support/genpredefmacrolist.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+#
+# Script to generate <items> list w/ all possible predefined gcc/g++ macros
+#
+
+default_options_file=`dirname $0`/`basename $0 .sh`.gcc-options
+
+function usage()
+{
+cat << __EOF
+Predefined gcc macros as kate syntax file (partly) generator v.001
+
+$0 [options]
+
+where options are:
+    -h          this help screen
+    -c file     gcc options file to use (if ommited '${default_options_file}')
+    -x          execute g++ instead of gcc
+
+Use CC environment variable to override default gcc executable.
+__EOF
+}
+
+# set default mode to C
+ext=".c"
+
+# Parse command line options
+while getopts "hc:x" option; do
+    case $option in
+    h)  usage
+        exit 0
+        ;;
+    x)  ext=".cc"
+        ;;
+    c)  option_files="$option_files $OPTARG"
+        ;;
+    *)  echo "*** Error: Unrecognized option" >&2
+        exit 1
+        ;;
+    esac
+done
+shift $(($OPTIND - 1))
+
+# Load gentoo spam functions
+if [ -f /etc/init.d/functions.sh ]; then
+    source /etc/init.d/functions.sh
+else
+    echo "*** Error: Sorry, this script for gentoo... I'm too lazy"
+    exit 1
+fi
+
+# Make temp files
+ebegin "Makeing temp files" >&2
+    tmp=`mktemp --tmpdir \`basename $0 .sh\`.XXXXXXXXXXX`
+    tmptmp=`mktemp --tmpdir \`basename $0 .sh\`.XXXXXXXXXXX`
+    tmp_src=`mktemp --tmpdir \`basename $0 .sh\`.XXXXXXXXXXX$ext`
+eend $? >&2
+
+# Verify options file
+test -z "$option_files" && option_files="$default_options_file"
+ebegin "Using options file(s): $option_files" >&2
+for cf in $option_files; do
+    if [ -r $cf ]; then
+        eend 0 >&2
+    else
+        eend 1 >&2
+        eerror "Unable to access options file $cf" >&2
+        exit 1
+    fi
+done
+
+# Find desired gcc binary (w/ default to `gcc`)
+gcc_bin=${CC:gcc}
+gcc_bin=`which "$gcc_bin" 2>/dev/null`
+if [ -x "$gcc_bin" ]; then
+    einfo "Using gcc: $gcc_bin" >&2
+else
+    eerror "Unable to find executable gcc binary" >&2
+    exit 1
+fi
+
+# Do the job!
+einfo "Starting..." >&2
+eindent
+    sed -e 's,\([^#]*\)#.*,\1,' -e '/^ *$/ d' $option_files | while read line; do
+        ebegin "Trying $line" >&2
+            rm "$tmptmp" 2>/dev/null
+            $gcc_bin $line -Werror -Wfatal-errors -dM -E -o "$tmptmp" "$tmp_src"
+            test -s "$tmptmp"
+        eend $? >&2
+        if [ -s "$tmptmp" ]; then
+            sed 's,#define \([^ (]*\).*,<item> \1 </item>,' "$tmptmp" >> "$tmp"
+        else
+            ewarn "Empty result"
+        fi
+    done
+eoutdent
+
+einfo "Remove temporary files" >&2
+rm "$tmptmp" "$tmp_src" 2>/dev/null
+
+# Show result. U may use >1 redirect to catch it into some file...
+sort "$tmp" | uniq
+rm $tmp
